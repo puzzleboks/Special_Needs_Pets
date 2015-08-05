7500c378ed465732a46d020f537c1a42b7f80c01
lon phelps <puzzleboks@gmail.com>
2015-08-05 09:24:09 -0400

    Created seeds file with default information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Pet.destroy_all
+Need.destroy_all
+Treatment.destroy_all
+
+
+puzzle = Pet.create(name: "Puzzle", breed: "English Springer Spaniel", pict_url: "puzzle.jpg", age: 10, weight: "50 lbs", sex: male)
+
+galaxy = Pet.create(name: "Galaxy", breed: "Siamese", pict_url: "galaxy.jpg", age: 2, weight: "10 lbs", sex: "female")
+
+tilda = Pet.create(name: "Tilda", breed: "Golden Retriever", pict_url: "tilda.jpg", age: 10, weight: "55 lbs", sex: "female")
+
+epilepsy = Need.create(kind: "Epilepsy", symptoms: "Seizures")
+
+zonisamide = Treatment.create(medicine: "Zonisamide", dosage: "2 tablets", every: "12 hours")
+
+keppra = Treatment.create(medicine: "Keppra", dosage: "2 tablets", every: "8 hours")
+
+gebapentin = Treatment.create(medicine: "Gebapentin", dosage: "1 tablets", every: "8 hours")
+
+phenobarbitol = Treatment.create(medicine: "Phenobarbitol", dosage: "1.5 tablets", every: "8 hours")
